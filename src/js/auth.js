0dbcf32fdabe9f17ff45e9bf9458c58376092ecd
Andrei Mikhalenia <mikhalenia.a@gmail.com>
2024-05-02 18:02:23 +0300

    fix: add error handling

--- src/js/auth.js
+++ src/js/auth.js
@@ -11,6 +11,18 @@ export function isLoginPage() {
     || /sign-up\.html$/.test(window.location.href);
 }
 
+export function addError(message) {
+  document.getElementById('errorContainer').innerHTML = `
+  <div class="alert alert-danger" role="alert">
+    ${message}
+  </div>
+`;
+}
+
+export function cleanError() {
+  document.getElementById('errorContainer').innerHTML = '';
+}
+
 export function setAuthChecker() {
   const auth = getApplicationAuth();
   onAuthStateChanged(auth, (authUser) => {
@@ -21,13 +33,25 @@ export function setAuthChecker() {
 }
 
 export async function signUp(email, password) {
-  const auth = getApplicationAuth();
-  return await createUserWithEmailAndPassword(auth, email, password);
+  try {
+    cleanError();
+    const auth = getApplicationAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+    window.location.href = '/index.html';
+  } catch (e) {
+    addError(e.message);
+  }
 }
 
 export async function signIn(email, password) {
-  const auth = getApplicationAuth();
-  return await signInWithEmailAndPassword(auth, email, password);
+  try {
+    cleanError();
+    const auth = getApplicationAuth();
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = '/index.html';
+  } catch (e) {
+    addError(e.message);
+  }
 }
 
 export function initSignUpPage() {
@@ -45,8 +69,6 @@ export function initSignUpPage() {
     }
 
     await signUp(userEmail, userPassword);
-
-    window.location.href = '/index.html';
   });
 }
 
@@ -65,8 +87,6 @@ export function initSignInPage() {
     }
 
     await signIn(userEmail, userPassword);
-
-    window.location.href = '/index.html';
   });
 }
 
