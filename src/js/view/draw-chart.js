49df7c9e036882d0e6d432ec3cb35f71e1e15445
Valentina-Kipel <167758501+Valentina-Kipel@users.noreply.github.com>
2024-05-13 18:58:51 +0200

    Draw charts done

--- src/js/view/draw-chart.js
+++ src/js/view/draw-chart.js
@@ -5,6 +5,7 @@ import {
   ChartTypeHistogramChart
 } from '../services/consts/charts';
 
+// Draw Bar Chart
 export function drawBarChart(data) {
   const preparedData = data.filter((x) => x.Year && x.Month);
   const ctx = document.getElementById('chart');
@@ -36,12 +37,59 @@ export function drawBarChart(data) {
   });
 }
 
+// Draw Pie Chart
 export function drawPieChart(data) {
+  const preparedData = data.filter((x) => x.Profit && x.Loss);
+  const ctx = document.getElementById('chart');
 
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: [
+        'Profit',
+        'Loss'
+      ],
+      datasets: [{
+        label: 'Profit',
+        data: [preparedData.map((x) => x.Profit), preparedData.map((x) => x.Loss)],
+        backgroundColor: [
+          'rgb(255,255,0)',
+          'rgb(255, 99, 132)'  
+        ],
+        hoverOffset: 4
+      }]
+    }
+  });
 }
 
 export function drawHistogramChart(data) {
+  const preparedData = data.filter((x) => x.Year);
+  const preparedDataRevenue = data.filter((x) => x.Revenue);
+  const ctx = document.getElementById('chart');
 
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: preparedData.map((x) => x.Year),
+      datasets: [{
+        label: 'Revenue per year',
+        data: preparedDataRevenue.map((x) => x.Revenue),
+        fill: true,
+        borderColor: 'blue', //
+        borderWidth: 5, // 
+        tension: 0, 
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          stacked: true,
+          showLine: true
+        }
+      }
+    }
+  });
 }
 
 export function drawChart(type, data) {
